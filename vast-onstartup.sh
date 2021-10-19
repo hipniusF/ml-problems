12d2b030787303ae0157c0ed068382cdbb813b70
Marco <marco@marcopg.com>
2021-10-19 13:29:32 +0200

    vast onstartup

---
+++ vast-onstartup.sh
@@ -0,0 +1,9 @@
+apt install -y git
+
+pip install ipympl
+pip install nodejs
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter labextension install jupyter-matplotlib
+jupyter nbextension enable --py widgetsnbextension
+
+git clone https://github.com/hipniusF/ml-problems.git

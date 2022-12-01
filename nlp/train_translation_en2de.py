e95ae41c40c443a22bba2e9d117ed2c71bfd2517
Marco <marco@marcopg.com>
2022-12-01 20:04:10 +0100

    train script

---
+++ nlp/train_translation_en2de.py
@@ -0,0 +1,20 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchtext import data, datasets
+import spacy
+from matplotlib import pyplot as plt
+import numpy as np
+from tqdm.auto import tqdm
+
+dev = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+from utils import Multi30KEn2DeDatasetTokenizer, get_trainer_model
+from transformers.model import EncoderDecoder, Trainer
+
+
+if __name__ == '__main__':
+    dataset = Multi30KEn2DeDatasetTokenizer(dev=dev)
+    trainer, model = get_trainer_model(dataset, EncoderDecoder, Trainer, dev=dev)
+    trainer.load('./current.pt')
+    trainer.train_loop(1_000_000, batch_size=400, save=True, notify=True)

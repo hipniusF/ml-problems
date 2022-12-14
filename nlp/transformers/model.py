fb8cfbcb725c0ff5d1b963b4fa28b9baaf7a4dcd
Marco <marco@marcopg.com>
2022-12-14 18:22:10 +0100

    avoid unk in loss calculation

--- nlp/transformers/model.py
+++ nlp/transformers/model.py
@@ -225,10 +225,11 @@ class EncoderDecoder(nn.Module):
 
 class LabelSmoothing(nn.Module):
     "Implement label smoothing."
-    def __init__(self, size, padding_idx=1, smoothing=0.0):
+    def __init__(self, size, padding_idx=1, unk_idx=None, smoothing=0.0):
         super(LabelSmoothing, self).__init__()
         self.criterion = nn.KLDivLoss(reduction="sum")
         self.padding_idx = padding_idx
+        self.unk_idx = unk_idx
         self.confidence = 1.0 - smoothing
         self.smoothing = smoothing
         self.size = size
@@ -240,9 +241,12 @@ class LabelSmoothing(nn.Module):
         true_dist.fill_(self.smoothing / (self.size - 2))
         true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
         true_dist[:, self.padding_idx] = 0
-        mask = torch.nonzero(target.data == self.padding_idx)
-        if mask.dim() > 0:
-            true_dist.index_fill_(0, mask.squeeze(), 0.0)
+        mask1 = torch.nonzero(target.data == self.padding_idx)
+        mask2 = torch.nonzero(target.data == self.unk_idx)
+        if mask1.dim() > 0:
+            true_dist.index_fill_(0, mask1.squeeze(), 0.0)
+        if mask2.dim() > 0:
+            true_dist.index_fill_(0, mask2.squeeze(), 0.0)
         self.true_dist = true_dist
         return self.criterion(x, true_dist.clone().detach())
 
@@ -279,6 +283,7 @@ class Trainer:
             return LabelSmoothing(
                 size=len(self.dataset.trg_vocab),
                 padding_idx=self.dataset.pad_symbol,
+                unk_idx=self.dataset.unk_symbol,
                 smoothing=0.1)
         else:
             raise ValueError(f'{criterion} is not a valid criterion')
